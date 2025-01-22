6d3e5f0ae57b867ee86ae6886938e49b7431f989
Mithlesh Yeole <mithlesh.yeole27@gmail.com>
2025-01-22 11:54:20 +0530

    Create UniqueArray.js
    Using function we are checking entered array and returning only elements entered which were unique in array and reject repeated elements

---
+++ UniqueArray.js
@@ -0,0 +1,19 @@
+let Uniquearray=[];
+const n= parseInt(prompt("enter the number of elements: "))
+
+for(let i=0; i<n; i++){
+    let ele = prompt('Enter element :');
+
+    let isdupe = false;
+    for(let j=0; j<Uniquearray.length; j++){
+        if(ele == Uniquearray[j]){
+            isdupe=true;
+            break;
+    }
+}
+if(!isdupe){
+    Uniquearray.push(ele);
+    }
+}
+console.log("unique array: ", Uniquearray); 
+
